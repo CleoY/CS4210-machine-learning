4d647d99665d108bebdd65eb2b0dc9dfe1831bc6
CleoY <77142560+CleoY@users.noreply.github.com>
2023-10-01 22:36:45 -0700

    Added basic code for enumerating datasets in X 4D feature matrix and Y class vector.

--- HW2/Q2/decision_tree_2.py
+++ HW2/Q2/decision_tree_2.py
@@ -7,6 +7,7 @@
 #-----------------------------------------------------------*/
 # IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. 
 # You have to work here only with standard dictionaries, lists, and arrays
+
 # Importing some Python libraries
 from sklearn import tree
 import csv
@@ -22,15 +23,56 @@ for ds in dataSets:
         for i, row in enumerate(reader):
             if i > 0: #skipping the header
                 dbTraining.append (row)
+                #print(row)
     
     # transform the original categorical training features to numbers and add to the 4D array X. For instance Young = 1, Prepresbyopic = 2, Presbyopic = 3
     # so X = [[1, 1, 1, 1], [2, 2, 2, 2], ...]]
-    #--> add your Python code here
-    # X =
+    for i, row in enumerate(dbTraining):
+        X.append([])
+        for col in range(len(row)-1):
+            tempItem = row[col]
+
+            # Age column
+            if col == 0:
+                if tempItem == "Young":
+                    X[i].append(1)
+                elif tempItem == "Prepresbyopic":
+                    X[i].append(2)
+                elif tempItem == "Presbyopic":
+                    X[i].append(3)
+            
+            # Spectacle prescription col
+            if col == 1:
+                if tempItem == "Myope":
+                    X[i].append(1)
+                elif tempItem == "Hypermetrope":
+                    X[i].append(2)
+
+            # Astigmatism col
+            if col == 2:
+                if tempItem == "Yes":
+                    X[i].append(1)
+                elif tempItem == "No":
+                    X[i].append(2)
+
+            # Tear production rate col
+            if col == 3:
+                if tempItem == "Normal":
+                    X[i].append(1)
+                elif tempItem == "Reduced":
+                    X[i].append(2)
+    print(X)
     
     #transform the original categorical training classes to numbers and add to the vector Y. For instance Yes = 1, No = 2, so Y = [1, 1, 2, 2, ...]
     #--> add your Python code here
-    # Y =
+    classLabelColIndex = 4
+    for row in dbTraining:
+        tempItem = row[classLabelColIndex]
+        if tempItem == "Yes":
+            Y.append(1)
+        elif tempItem == "No":
+            Y.append(2)
+    print(Y)
     
     #loop your training and test tasks 10 times here
     for i in range (10):
@@ -40,7 +82,7 @@ for ds in dataSets:
         #read the test data and add this data to dbTest
         #--> add your Python code here
         # dbTest =
-        for data in dbTest:
+        #for data in dbTest:
             #transform the features of the test instances to numbers following the same strategy done during training,
             #and then use the decision tree to make the class prediction. For instance: class_predicted = clf.predict([[3, 1, 2, 1]])[0]
             #where [0] is used to get an integer as the predicted class label so that you can compare it with the true label

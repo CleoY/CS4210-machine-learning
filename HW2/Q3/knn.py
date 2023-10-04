173c8c0ea868a42bad6b7e97757a5899cf4dd8a9
CleoY <77142560+CleoY@users.noreply.github.com>
2023-10-04 15:49:07 -0700

    Added code to place data in X feature matrix and Y class vector.

--- HW2/Q3/knn.py
+++ HW2/Q3/knn.py
@@ -12,24 +12,56 @@ import csv
 db = []
 
 #reading the data in a csv file
-with open('binary_points.csv', 'r') as csvfile:
+with open('./binary_points.csv', 'r') as csvfile:
     reader = csv.reader(csvfile)
     for i, row in enumerate(reader):
         if i > 0: #skipping the header
             db.append (row)
 
+# variable to exclude in training
+testInstance = 0
+
 #loop your data to allow each instance to be your test set
-for ? in db:
+for recordIndex, record in enumerate(db):
     #add the training features to the 2D array X removing the instance that will be used for testing in this iteration. 
     # For instance, X = [[1, 3], [2, 1,], ...]]. Convert each feature value to
     # float to avoid warning messages
-    #--> add your Python code here
-    # X =
+    X = []
+    
+    # j in X[j].append(tempItem) is wrong because some indices are skipped and would be out-of-range
+    # the index used to append data to X should be dif from the index of the database in the larger loop
+    # the same index should be kept for skipping the class label of the test instance when init Y
+    ####
+    xIndex = 0
+    for j, row in enumerate(db):
+        if(j != recordIndex):
+            X.append([])
+            # len(row) - 1 excludes the class label
+            for col in range(len(row)-1):
+                tempItem = float(row[col])
+                X[xIndex].append(tempItem)
+
+            xIndex+=1
+
+    #####
+    print("\nRecord #"+str(recordIndex))
+    print("X:")
+    print(X)
+    
     
     #transform the original training classes to numbers and add to the vector Y removing the instance that will be used for testing in this iteration. 
     # For instance, Y = [1, 2, ,...]. Convert each feature value to float to avoid warning messages
-    #--> add your Python code here
-    # Y =
+    Y = []
+    yIndex = 0
+    for k, row in enumerate(db):
+        if(k != recordIndex):
+            tempItem = row[len(row)-1]
+            if(tempItem == "+"):
+                Y.append(1.0)
+            elif(tempItem == "-"):
+                Y.append(2.0)
+    print("Y")
+    print(Y)
     
     #store the test sample of this iteration in the vector testSample
     #--> add your Python code here
@@ -44,6 +76,8 @@ for ? in db:
     
     #compare the prediction with the true label of the test instance to start calculating the error rate.
     #--> add your Python code here
+
+    testInstance+=1
 
 #print the error rate
 #--> add your Python code here

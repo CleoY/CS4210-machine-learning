d6edb895e30e3f1284dedc6071e0f0305da7b86c
CleoY <77142560+CleoY@users.noreply.github.com>
2023-10-04 16:36:15 -0700

    Enumerated weather_training dataset for db.

--- HW2/Q5/naive_bayes.py
+++ HW2/Q5/naive_bayes.py
@@ -1,7 +1,8 @@
 #-------------------------------------------------------------------------
 # AUTHOR: Cleo Yau
 # FILENAME: naive_bayes.py
-# SPECIFICATION: description of the program
+# SPECIFICATION: Train with weather_training dataset and classify weather_test
+#               dataset. Print classifications if prediction confidence >= 0.75
 # FOR: CS 4210 - Assignment #2
 # TIME SPENT: how long it took you to complete the assignment
 #-----------------------------------------------------------*/
@@ -10,13 +11,57 @@
 
 #importing some Python libraries
 from sklearn.naive_bayes import GaussianNB
+import csv
+
 #reading the training data in a csv file
-#--> add your Python code here
+db = []
+with open('./weather_training.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for i, row in enumerate(reader):
+        if i > 0: #skipping the header
+            db.append (row)
 
 #transform the original training features to numbers and add them to the 4D array X.
 # For instance: Sunny = 1, Overcast = 2, Rain = 3, so X = [[3, 1, 1, 2], [1, 3, 2,2], ...]]
-#--> add your Python code here
-# X =
+X = []
+for i, row in enumerate(db):
+    X.append([])
+    # Skip first Day column and last PlayTennis class label column
+    for col in range(1, len(row)-1):
+        tempItem = row[col]
+
+        # Outlook column
+        if col == 1:
+            if tempItem == "Sunny":
+                X[i].append(1)
+            elif tempItem == "Overcast":
+                X[i].append(2)
+            elif tempItem == "Rain":
+                X[i].append(3)
+        
+        # Temperature col
+        if col == 2:
+            if tempItem == "Hot":
+                X[i].append(1)
+            elif tempItem == "Mild":
+                X[i].append(2)
+            elif tempItem == "Cool":
+                X[i].append(3)
+
+        # Humidity col
+        if col == 3:
+            if tempItem == "High":
+                X[i].append(1)
+            elif tempItem == "Normal":
+                X[i].append(2)
+
+        # Wind col
+        if col == 4:
+            if tempItem == "Strong":
+                X[i].append(1)
+            elif tempItem == "Weak":
+                X[i].append(2)
+print(X)
 
 #transform the original training classes to numbers and add them to the vector Y.
 #For instance Yes = 1, No = 2, so Y = [1, 1, 2, 2, ...]

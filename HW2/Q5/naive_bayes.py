131f765f120e0f5efd71692ed7970be2881e9cac
CleoY <77142560+CleoY@users.noreply.github.com>
2023-10-04 20:22:32 -0700

    Finished naive_bayes.py code.

--- HW2/Q5/naive_bayes.py
+++ HW2/Q5/naive_bayes.py
@@ -4,7 +4,7 @@
 # SPECIFICATION: Train with weather_training dataset and classify weather_test
 #               dataset. Print predictions if classification confidence >= 0.75
 # FOR: CS 4210 - Assignment #2
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 2 hours
 #-----------------------------------------------------------*/
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard
 # dictionaries, lists, and arrays
@@ -61,7 +61,6 @@ for i, row in enumerate(db):
                 X[i].append(1)
             elif tempItem == "Weak":
                 X[i].append(2)
-print(X)
 
 #transform the original training classes to numbers and add them to the vector Y.
 #For instance Yes = 1, No = 2, so Y = [1, 1, 2, 2, ...]
@@ -72,7 +71,6 @@ for row in db:
         Y.append(1)
     elif tempItem == "No":
         Y.append(2)
-print(Y)
 
 #fitting the naive bayes to the data
 clf = GaussianNB()
@@ -85,7 +83,6 @@ with open('./weather_test.csv', 'r') as csvfile:
     for i, row in enumerate(reader):
         if i > 0: #skipping the header
             testDB.append (row)
-print(testDB)
 
 #enumerate the testing db
 enumTestDB = []
@@ -126,30 +123,15 @@ for i, row in enumerate(testDB):
                 enumTestDB[i].append(1)
             elif tempItem == "Weak":
                 enumTestDB[i].append(2)
-print(enumTestDB)
+
 
 #printing the header as the solution
 print("Day \t Outlook \t Temperature \t Humidity \t PlayTennis \t Confidence")
 
-
-# \/ feels like a bad way to get the headers. Still need ot print them nicely, too
-# headers = []
-# with open('./weather_test.csv', 'r') as csvfile:
-#     reader = csv.reader(csvfile)
-    
-#     for i, row in enumerate(reader):
-#         if i == 0:
-#            headers.append (row)
-#            break
-# print(headers)
-
-
-#use your test samples to make probabilistic predictions. For instance:
-#clf.predict_proba([[3, 1, 2, 1]])[0]
+#use your test samples to make probabilistic predictions. For instance: clf.predict_proba([[3, 1, 2, 1]])[0]
 for recordIndex, record in enumerate(enumTestDB):
     class_probabilities = clf.predict_proba([record])[0]
-    print("Class prob:")
-    print(class_probabilities)
+
     if(class_probabilities[0] >= 0.75):
         predicted_class = "Yes"
         confidence = class_probabilities[0]
@@ -159,9 +141,6 @@ for recordIndex, record in enumerate(enumTestDB):
     else:
         predicted_class = 0
         confidence = 0
-    
-    print("Predicted class:")
-    print(predicted_class)
 
     if(predicted_class != 0):
         print(testDB[recordIndex][0] + "\t" + testDB[recordIndex][1] + "\t" 
@@ -169,10 +148,3 @@ for recordIndex, record in enumerate(enumTestDB):
               + testDB[recordIndex][4] + "\t" + predicted_class + "\t"
               + str(confidence)
               )
-
-# for recordIndex, record in enumerate(enumTestDB):
-#     prob = clf.predict_proba(enumTestDB[recordIndex])[0]
-#     print(prob)
-# need index for enumTestDB to get testDb record at same index
-
-#print table of predictions with a classification confidence >= 0.75
